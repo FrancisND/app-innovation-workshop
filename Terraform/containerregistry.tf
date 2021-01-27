5aa07e7811b1754206ca8b5415501a86b0c31ad0
Francis Ndangi <francis.ndangi@datonomy.co.za>
2021-01-27 18:31:43 +0200

    initial

---
+++ Terraform/containerregistry.tf
@@ -0,0 +1,7 @@
+resource "azurerm_container_registry" "workshop" {
+  name                = "${var.prefix}${var.resource_name}registry"
+  resource_group_name = "${azurerm_resource_group.workshop.name}"
+  location            = "${azurerm_resource_group.workshop.location}"
+  admin_enabled       = true
+  sku                 = "Basic"
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "Choose a personal prefix (1-10 chars) that is attached to every resource to ensure a unique name."
+  default     = ""
+}
+
+variable "resource_name" {
+  default     = "contoso"
+  description = "Name of the project, which will be attached to every resource name."
+}
